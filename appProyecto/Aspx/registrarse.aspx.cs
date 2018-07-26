2582d299bf29858c7b4ceb59b891fefea850533d
José Pablo <pablrj2@outlook.com>
2018-07-26 14:10:58 -0600

    l

--- appProyecto/Aspx/registrarse.aspx.cs
+++ appProyecto/Aspx/registrarse.aspx.cs
@@ -1,17 +1,37 @@
-﻿using System;
+﻿using appProyecto.BD;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace appProyecto.Aspx
 {
     public partial class registrarse : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        /*protected void Page_Load(object sender, EventArgs e)
         {
+        
+            if (!IsPostBack)
+            {
+                RepeaterProductos.DataSource =
+                    Registrar.SeleccionarTodos();
+                RepeaterProductos.DataBind();
+            }
+        }*/
 
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            Registrar cat = new Registrar();
+            cat.nombre = nombre.Value;
+            cat.contraseña = contraseña.Value;
+            cat.correo = correo.Value;
+            cat.pais = pais.Value;
+            cat.identificacion = identificacion.Value;
+            Registrar.Insertar(cat);
+            Response.Redirect("registrarse.aspx");
         }
     }
-}
+    }
---
+++ appProyecto/BD/Database.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace appProyecto.BD
+{
+    class Database : IDisposable
+    {
+        /*IDisposable permite a los objetos liberar los recursos que tengan correctamente y de forma determinista. 
+         * Si en el código anteriorprodujese una excepción se corre el riesgo de no liberar los recursos. 
+         * En el mejor de los casos, esos recursos se liberarían
+         * y en el peor de los casos nunca se liberarían.*/
+        public SqlConnection Conexion { get; set; }      
+
+        public DataSet ExecuteReader(SqlCommand sqlCommand, String tabla)
+        {
+
+            DataSet dsTabla = new DataSet();
+            try
+            {
+                using (SqlDataAdapter adaptador = new SqlDataAdapter(sqlCommand))
+                {
+                    sqlCommand.Connection = Conexion;
+                    dsTabla = new DataSet();
+                    adaptador.Fill(dsTabla, tabla);
+                }
+                return dsTabla;
+            }
+            catch (Exception ex)
+            {
+                ex.Source += " SQL: " + sqlCommand.CommandText.ToString();
+                throw ex;
+            }
+            finally
+            {
+
+                if (dsTabla != null)
+                    dsTabla.Dispose();
+
+
+            }
+
+
+
+        }
+        
+        public int ExecuteNonQuery(SqlCommand sqlCommand)
+        {
+
+            int registrosafectados = 0;
+            try
+            {
+
+                sqlCommand.Connection = Conexion;
+
+                registrosafectados = sqlCommand.ExecuteNonQuery();
+
+                return registrosafectados;
+
+            }
+            catch (Exception ex)
+            {
+                ex.Source += " SQL: " + sqlCommand.CommandText.ToString();
+                throw ex;
+            }
+        }
+
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            if (Conexion != null)
+                Conexion.Close();
+        }
+
+        #endregion
+    }
+}
